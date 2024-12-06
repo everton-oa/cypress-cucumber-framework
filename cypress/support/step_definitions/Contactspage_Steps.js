90382f0b2b6d8ff8f4c3cd82801d57eca4230e92
Everton Araujo <everton.oa@gmail.com>
2024-12-06 07:30:40 +0000

    :construction: [CL-19] Add stepDefinitions

---
+++ cypress/support/step_definitions/Contactspage_Steps.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+const { When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+
+When(`I type a first name`, () => {
+});
+
+When(`I type a last name`, () => {
+});
+
+When(`I enter an email address`, () => {
+});
+
+When(`I type a comment`, () => {
+});
+
+Then(
+  `I click on the submit buttonThen I should be presented with a successful contact us submission message`,
+  () => {
+  }
+);
+
