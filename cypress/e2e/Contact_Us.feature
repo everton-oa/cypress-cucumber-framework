30fad2ea647e9ba16d36407919f9eb56e04fe2e1
Everton Araujo <everton.oa@gmail.com>
2024-12-06 07:56:22 +0000

    :construction: [CL-20] Add stepDefinitions

--- cypress/e2e/Contact_Us.feature
+++ cypress/e2e/Contact_Us.feature
@@ -8,4 +8,5 @@ And I type a first name
 And I type a last name
 And I enter an email address
 And I type a comment
-And I click on the submit buttonThen I should be presented with a successful contact us submission message
+And I click on the submit button
+Then I should be presented with a successful contact us submission message
--- cypress/support/step_definitions/Contactspage_Steps.js
+++ cypress/support/step_definitions/Contactspage_Steps.js
@@ -2,20 +2,28 @@
 const { When, Then } = require("@badeball/cypress-cucumber-preprocessor");
 
 When(`I type a first name`, () => {
+  cy.get('[name="first_name"]').type("Test");
 });
 
 When(`I type a last name`, () => {
+  cy.get('[name="last_name"]').type("Cypress");
 });
 
 When(`I enter an email address`, () => {
+  cy.get('[name="email"]').type("test@cypress.com");
 });
 
 When(`I type a comment`, () => {
+  cy.get("textarea[name='message']").type("Hello World");
 });
 
-Then(
-  `I click on the submit buttonThen I should be presented with a successful contact us submission message`,
+When(
+  `I click on the submit button`,
   () => {
+    cy.get('[type="submit"]').click();
   }
 );
 
+Then(`I should be presented with a successful contact us submission message`, () => {
+  cy.get("h1").should("have.text", "Thank You for your Message!");
+});
